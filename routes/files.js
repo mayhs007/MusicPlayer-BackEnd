e8776ef27da1d7169bb83fa784e069f14259355b
gyanmitra19 <45943884+gyanmitra19@users.noreply.github.com>
2019-02-16 16:15:44 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -5,68 +5,74 @@ var fs = require('fs');
 var path = require('path');
 const NodeID3 = require('node-id3');
 const { getAudioDurationInSeconds } = require('get-audio-duration');
-const ffmpeg = require('fluent-ffmpeg')
-const probe = require('ffmpeg-probe')
-
+const ffmpeg = require('fluent-ffmpeg');
+const probe = require('ffmpeg-probe');
+var encoding = "utf8";
 router.get('/getFiles', (req, res) => {
+
     var dirTree = ('E:\MUSIC\\Tamil\\Vantha-Rajavathaan-Varuven');
+    //console.log(dirTree);
+    const path2 = "./music.json";
+    if (fs.existsSync(path2)) {
+        console.log("Working");
+        var contents = fs.readFileSync('music.json', 'utf8');
+        res.send(contents);
+    } else {
+        var diretoryTreeToObj = function(dir, done) {
+            var results = [];
+            var trackNumber = 0;
+            fs.readdir(dir, function(err, list) {
+                if (err)
+                    return done(err);
 
-    console.log(dirTree);
+                var pending = list.length;
+                if (!pending)
+                    return done(null, { name: path.basename(dir), type: 'folder', children: results });
 
-    var diretoryTreeToObj = function(dir, done) {
-        var results = [];
-        var trackNumber = 0;
-
-        fs.readdir(dir, function(err, list) {
-            if (err)
-                return done(err);
-
-            var pending = list.length;
-
-            if (!pending)
-                return done(null, { name: path.basename(dir), type: 'folder', children: results });
-
-            list.forEach(function(file) {
-                file = path.resolve(dir, file);
-                fs.stat(file, function(err, stat) {
-                    if (stat && stat.isDirectory()) {
-                        diretoryTreeToObj(file, function(err, data) {
-                            results.push({
-                                name: path.basename(file),
-                                type: 'folder',
-                                children: data
+                list.forEach(function(file) {
+                    file = path.resolve(dir, file);
+                    fs.stat(file, function(err, stat) {
+                        if (stat && stat.isDirectory()) {
+                            diretoryTreeToObj(file, function(err, data) {
+                                results.push({
+                                    name: path.basename(file),
+                                    type: 'folder',
+                                    children: data
+                                });
+                                if (!--pending)
+                                    done(null, results);
                             });
-                            if (!--pending)
-                                done(null, results);
-                        });
-                    } else {
+                        } else {
 
-                        mp3Duration(file, function(err, duration) {
-                            duration = duration / 60;
-                            results.push({
-                                type: path.parse(file).ext,
-                                file: path.basename(file),
-                                name: path.parse(file).name,
-                                duration: duration.toFixed(2),
-                                trackNumber: trackNumber++
+                            mp3Duration(file, function(err, duration) {
+                                duration = duration / 60;
+                                results.push({
+                                    type: path.parse(file).ext,
+                                    file: path.basename(file),
+                                    name: path.parse(file).name,
+                                    duration: duration.toFixed(2),
+                                    trackNumber: trackNumber++
+                                });
+                                if (!--pending)
+                                    done(null, results);
                             });
-                            if (!--pending)
-                                done(null, results);
-                        });
-
-
-                    }
+                        }
+                    });
                 });
             });
-        });
-    };
-    diretoryTreeToObj(dirTree, function(err, data) {
-        if (err)
-            console.error(err);
-        //console.log(JSON.stringify(data));
-        // res.send(res);
-        res.send(data);
+        };
+        diretoryTreeToObj(dirTree, function(err, data) {
+            if (err)
+                console.error(err);
+            var data = JSON.stringify(data);
+            fs.writeFile('music.json', data, encoding, (err) => {
+                if (err) throw err;
+                console.log("The file was succesfully saved!");
+            });
+            res.send(data);
 
-    });
+        });
+    }
+
 });
-module.exports = router
+module.exports = router;
